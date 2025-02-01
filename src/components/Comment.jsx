0704d96e939929d61deb5054fca6ded14eb417ed
Arthur Lima <artglima.dev@gmail.com>
2025-02-01 13:12:15 -0300

    feat: stylization Comment component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,20 +1,22 @@
+import styles from './Comment.module.css'
+
 import { ThumbsUp, Trash } from '@phosphor-icons/react'
 
 export function Comment() {
   return (
-    <div className="comment">
+    <div className={styles.comment}>
       <img src="https://avatars.githubusercontent.com/u/66640568?v=4" alt="" />
 
-      <div className="commentBox">
-        <div className="commentContent">
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
           <header>
-            <div className="authorAndTime">
+            <div className={styles.authorAndTime}>
               <strong>Arthur Lima</strong>
               <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
             </div>
 
             <button title="Deletar comentário">
-              <Trash size={20} />
+              <Trash size={24} />
             </button>
           </header>
 
