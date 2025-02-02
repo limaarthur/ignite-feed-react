b0e363f99e8f2256bb501bebcaebef8260ac82dc
Arthur Lima <artglima.dev@gmail.com>
2025-02-02 10:24:15 -0300

    feat: inserting comments with declarative programming

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,7 @@ import styles from './Comment.module.css'
 
 import { ThumbsUp, Trash } from '@phosphor-icons/react'
 
-export function Comment() {
+export function Comment({ content }) {
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/66640568?v=4" />
@@ -21,7 +21,7 @@ export function Comment() {
             </button>
           </header>
 
-          <p>Muito bom Devon, parabéns!! 👏👏</p>
+          <p>{content}</p>
         </div>
 
         <footer>
