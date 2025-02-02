5587c3f430f7a17f41e8286a117a9e9d483a0496
Arthur Lima <artglima.dev@gmail.com>
2025-02-02 01:59:35 -0300

    feat: iterating in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
 import { Header } from './components/Header';
 
 import styles from './App.module.css'
-import './global.css';
+
 import { Seidebar } from './components/Sidebar';
 import { Post } from './components/Post';
+
+import './global.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: `https://avatars.githubusercontent.com/u/66640568?v=4`,
+      name: 'Arthur Lima',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/62348132?v=4',
+      name: 'Gustavo Souza',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00'),
+  },
+];
 
 export function App() {
   return (
@@ -12,7 +45,18 @@ export function App() {
 
       <div className={styles.wrapper}>
         <Seidebar />
-        <Post />
+
+        <main>
+          {posts.map(posts => {
+            return (
+              <Post 
+                author={posts.author}
+                content={posts.content}
+                publishedAt={posts.publishedAt}
+              />
+            )
+          })}
+        </main>
       </div>
     </div>
   )
