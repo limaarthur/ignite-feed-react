a7601d7bf9cc70db1ccb43c71757171428eb29a5
Arthur Lima <artglima.dev@gmail.com>
2025-02-02 10:45:38 -0300

    feat: solving key props

--- src/App.jsx
+++ src/App.jsx
@@ -47,12 +47,13 @@ export function App() {
         <Seidebar />
 
         <main>
-          {posts.map(posts => {
+          {posts.map(post => {
             return (
               <Post 
-                author={posts.author}
-                content={posts.content}
-                publishedAt={posts.publishedAt}
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
               />
             )
           })}
